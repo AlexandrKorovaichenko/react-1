34496449e16cf51fe15993f063b1b77ea26f706d
Alexandr Korovaychenko <puls.ek@rambler.ru>
2022-03-27 20:13:00 +0300

    React - Путь самурая. Уроки 1-80

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React            from 'react';
+import Navbar           from './components/Navbar/Navbar';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import News             from './components/News/News';
+import Music            from './components/Music/Music';
+import Setting          from './components/Setting/Setting';
+import UsersContainer   from './components/Users/UsersContainer';
+import HeaderContainer  from './components/Header/HeaderContainer';
+import Login            from "./components/Login/Login"
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+const App = () => {
+
+    //<Route path='/profile/*'  element = { <Profile store={props.store} /> } />
+    //<Route path='/dialogs/*'  element = { <DialogsContainer store={props.store} /> } />
+    
+    return (
+
+      <BrowserRouter>
+        <div className='app-wraper'>
+          
+            <HeaderContainer />     
+            <Navbar />
+          
+            <div className="app-wraper-content">
+              
+            <Routes>
+
+                <Route path='/profile'  element = { <ProfileContainer /> } />
+                <Route path='/profile/:userId'  element = { <ProfileContainer /> } />
+                
+                <Route path='/dialogs/*'  element = { <DialogsContainer /> } />
+                <Route path='/users/*'    element = { <UsersContainer /> } />
+                                                    
+                <Route path='/news/*'     element = {<News />} />
+                <Route path='/music/*'    element = {<Music />} />
+                <Route path='/setting/*'  element = {<Setting />} />
+                <Route path='/login/*'    element = {<Login />} />
+                
+            </Routes> 
+
+            </div>
+ 
+        </div>
+      </BrowserRouter>
+      
+      
+    );
 }
 
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
+import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './redux/redux-store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  /*let rerenderEntireTree = (state) => {*/
+
+  // Этот стор и попадает в App через <Provider store = {store} > 
+
+  ReactDOM.render(
+      <React.StrictMode>
+
+        <Provider store = {store} >
+          <App />
+        </Provider>
+
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+
+
+/*}
+
+rerenderEntireTree(store.getState());
+store.subscribe(()=> { 
+  let state = store.getState();
+  rerenderEntireTree(state); 
+});*/
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
